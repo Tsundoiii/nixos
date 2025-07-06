ae0d46724077f619fe47878b9750abdd09da04e9
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-07-06 01:22:05 -0400

    add vscode settings

--- home/programs/programming/vscode.nix
+++ home/programs/programming/vscode.nix
@@ -4,8 +4,20 @@
   programs.vscode = {
     enable = true;
     
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      bbenoist.nix
-    ];
+    profiles.default = {
+      userSettings = {
+        "git.autofetch" = true;
+        "editor.formatOnSave" = true;
+        "editor.formatOnPaste" = true;
+        "editor.formatOnType" = true;
+        "editor.bracketPairColorization.independentColorPoolPerBracketType" = true;
+        "files.autoSave" = "onFocusChange";
+        "telemetry.telemetryLevel" = "error";
+      };
+
+      extensions = with pkgs.vscode-extensions; [
+        bbenoist.nix
+      ];
+    };
   };
 }

d741ad05e4c574ddf7244772b4a77b1b3ee87c3c
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-07-10 12:44:24 -0400

    add scanner

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -12,15 +12,24 @@
     drivers = [ pkgs.hplipWithPlugin ];
   };
 
-  hardware.printers = {
-    ensurePrinters = [
-      {
-        name = "Printer";
-        location = "Home";
-        model = "HP/hp-laserjet_professional_m1212nf_mfp.ppd.gz";
-        deviceUri = "usb://HP/LaserJet%20Professional%20M1212nf%20MFP?serial=000000000QJ46WG1PR1a";
-      }
-    ];
-    ensureDefaultPrinter = "Printer";
+  hardware = {
+    printers = {
+      ensurePrinters = [
+        {
+          name = "Printer";
+          location = "Home";
+          model = "HP/hp-laserjet_professional_m1212nf_mfp.ppd.gz";
+          deviceUri = "usb://HP/LaserJet%20Professional%20M1212nf%20MFP?serial=000000000QJ46WG1PR1a";
+        }
+      ];
+      ensureDefaultPrinter = "Printer";
+    };
+
+    sane = {
+      enable = true;
+      extraBackends = [ pkgs.hplipWithPlugin ];
+    };
   };
+
+  environment.systemPackages = [ pkgs.simple-scan ];
 }

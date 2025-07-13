af76734d602c689f99bc60a99be62ff4d1f54d89
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-07-13 14:16:06 -0400

    update hyprland config

--- home/programs/display/hyprland.nix
+++ home/programs/display/hyprland.nix
@@ -1,20 +1,31 @@
 { pkgs, ... }:
 
 {
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
+  wayland.windowManager.hyprland = {
+    enable = true;
 
-    bind = [
-      "$mod, F, exec, firefox"
-    ] ++ (
-      builtins.concatLists (builtins.genList (i: 
-        let workspace = i + 1;
-        in [
-          "$mod, code1${toString i}, workspace, ${toString workspace}"
-          "$mod SHIFT, code1${toString i}, movetoworkspace, ${toString workspace}"
-        ]
-      )
-      9)
-    );
+    settings = {
+      "$mod" = "SUPER";
+      "$terminal" = "kitty";
+
+      exec-once = [ "firefox" "discord" ];
+    
+      bind = [
+        "$mod, C, killactive,"
+        "$mod, Q, exec, $terminal"
+        "$mod, F, exec, firefox"
+        "$mod, D, exec, discord"
+        "$mod, V, exec, code"
+      ] ++ (
+        builtins.concatLists (builtins.genList (i: 
+          let workspace = i + 1;
+          in [
+            "$mod, ${toString workspace}, workspace, ${toString workspace}"
+            "$mod SHIFT, ${toString workspace}, movetoworkspace, ${toString workspace}"
+          ]
+        )
+        9)
+      );
+    };
   };
 }

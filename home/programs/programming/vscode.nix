17ce7bc2c4cb0d4e66b83d916360a688c624cd9f
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-09-09 19:43:41 -0400

    update vscode git setting

--- home/programs/programming/vscode.nix
+++ home/programs/programming/vscode.nix
@@ -8,10 +8,13 @@
       userSettings = {
         "git.autofetch" = true;
         "git.confirmSync" = false;
+        "git.openRepositoryInParentFolders" = "always";
+
         "editor.formatOnSave" = true;
         "editor.formatOnPaste" = true;
         "editor.formatOnType" = true;
         "editor.bracketPairColorization.independentColorPoolPerBracketType" = true;
+
         "files.autoSave" = "onFocusChange";
         "explorer.confirmDragAndDrop" = false;
         "telemetry.telemetryLevel" = "error";

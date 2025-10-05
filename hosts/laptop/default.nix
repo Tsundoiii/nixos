701e90a1710aabf2d1638df555beffd2ef1cfda0
Tsundoiii <91398247+Tsundoiii@users.noreply.github.com>
2025-10-05 00:47:16 -0400

    add bluetooth

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -5,20 +5,27 @@
 
   networking.hostName = "laptop";
 
-  hardware.printers = {
-    ensurePrinters = [
-      {
-        name = "itap-printing";
-        description = "Purdue Black and White Printing";
-        deviceUri = "lpd://chen5708@wpvapppcprt02.itap.purdue.edu:515/itap-printing";
-        model = "drv:///sample.drv/generic.ppd";
-      }
-      {
-        name = "itap-colorprinting";
-        description = "Purdue Black and White Printing";
-        deviceUri = "lpd://chen5708@wpvapppcprt02.itap.purdue.edu:515/itap-colorprinting";
-        model = "drv:///sample.drv/generic.ppd";
-      }
-    ];
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+
+    printers = {
+      ensurePrinters = [
+        {
+          name = "itap-printing";
+          description = "Purdue Black and White Printing";
+          deviceUri = "lpd://chen5708@wpvapppcprt02.itap.purdue.edu:515/itap-printing";
+          model = "drv:///sample.drv/generic.ppd";
+        }
+        {
+          name = "itap-colorprinting";
+          description = "Purdue Black and White Printing";
+          deviceUri = "lpd://chen5708@wpvapppcprt02.itap.purdue.edu:515/itap-colorprinting";
+          model = "drv:///sample.drv/generic.ppd";
+        }
+      ];
+    };
   };
 }

b8574b3f3ae1acb45679e026d7f2d91508789d33
Tsundoiii <dev@tsundoiii.dev>
2026-08-02 22:51:31 -0400

    add niri flake cache

--- system/nix.nix
+++ system/nix.nix
@@ -3,6 +3,8 @@
 {
   nix.settings = {
     warn-dirty = false;
+    trusted-substituters = [ "https://niri.cachix.org" ];
+    trusted-public-keys = [ "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964=" ];
 
     experimental-features = [
       "nix-command"

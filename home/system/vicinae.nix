317278b77a950eee5f78c74622840c5eb09a2655
Tsundoiii <dev@tsundoiii.dev>
2026-08-06 14:26:58 -0400

    add vicinae favorites

--- home/system/vicinae.nix
+++ home/system/vicinae.nix
@@ -2,5 +2,12 @@
   programs.vicinae = {
     enable = true;
     systemd.enable = true;
+
+    settings.favorites = [
+      "power:power-off"
+      "power:reboot"
+      "power:hibernate"
+      "system:run"
+    ];
   };
 }
